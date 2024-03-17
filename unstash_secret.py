ce0f99e35269fba92722331c015c42640452a17e
aster pieters <aster.ieters@gmail.com>
2024-03-17 17:16:53 +0100

    added confirmation

--- unstash_secret.py
+++ unstash_secret.py
@@ -1,9 +1,11 @@
 from sql import execute_query
+from peek_stash import show_entry_by_id
 
 def unstash_secret(id, force_delete, password):
 
     if not force_delete:
-        user_input = input('Are you sure? (y/N)')
+        show_entry_by_id(password, id)
+        user_input = input('Are you sure? (y/N) ')
         if user_input != 'y':
             return
 
