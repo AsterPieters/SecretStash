80b853bb3fe6ed9b1c3af5c02643023a0d845b06
aster pieters <aster.ieters@gmail.com>
2024-03-19 23:46:15 +0100

    idk

--- unstash_secret.py
+++ unstash_secret.py
@@ -5,7 +5,7 @@ def unstash_secret(id, force_delete, password):
 
     if not force_delete:
         show_entry_by_id(password, id)
-        user_input = input('Are you sure? (y/N) ')
+        user_input = input('This entry will be permanently deleted,\nAre you sure? (y/N) ')
         if user_input != 'y':
             return
 
